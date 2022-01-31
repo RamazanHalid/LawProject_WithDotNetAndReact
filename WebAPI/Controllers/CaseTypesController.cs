7c3dd99adf1bffe4f2f7e7d91f073a35863ba3ec
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-01-31 17:55:29 +0300

    CaseType added to the WebApi layer

---
+++ WebAPI/Controllers/CaseTypesController.cs
@@ -0,0 +1,80 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaseTypesController : ControllerBase
+    {
+        private ICaseTypeService _caseTypeService;
+
+        public CaseTypesController(ICaseTypeService caseTypeService)
+        {
+            _caseTypeService = caseTypeService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _caseTypeService.GetAll();
+            if (result.Success)
+            {
+               return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _caseTypeService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _caseTypeService.Delete(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("ChangeActivity")]
+        public IActionResult ChangeActivity(int id)
+        {
+            var result = _caseTypeService.ChangeActivity(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(CaseType caseType)
+        {
+            var result = _caseTypeService.Add(caseType);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Update")]
+        public IActionResult Update(CaseType caseType)
+        {
+            var result = _caseTypeService.Update(caseType);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
