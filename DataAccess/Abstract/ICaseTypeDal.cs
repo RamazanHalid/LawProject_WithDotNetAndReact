f6c3978aab4024589597ad0b97ea6fb32651ae6f
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-01-31 17:54:28 +0300

    CaseType added to the DataAccess Layer

---
+++ DataAccess/Abstract/ICaseTypeDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICaseTypeDal : IEntityRepository<CaseType>
+    {
+        List<CaseType> GetAllWithCourtOfficeType(Expression<Func<CaseType, bool>> filter = null);
+        CaseType GetByIdWithCourtOfficeType(Expression<Func<CaseType, bool>> filter);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCaseTypeDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCaseTypeDal : EfEntityRepositoryBase<CaseType, HukukContext>, ICaseTypeDal
+    {
+        public List<CaseType> GetAllWithCourtOfficeType(Expression<Func<CaseType, bool>> filter = null)
+        {
+            using (var context = new HukukContext())
+            {
+                return filter == null
+                    ? context.Set<CaseType>().Include(ct => ct.CourtOfficeType).ToList()
+                    : context.Set<CaseType>().Where(filter).Include(ct => ct.CourtOfficeType).ToList();
+            }
+        }
+
+        public CaseType GetByIdWithCourtOfficeType(Expression<Func<CaseType, bool>> filter)
+        {
+            using (var context = new HukukContext())
+            {
+                return context.Set<CaseType>().Include(ct => ct.CourtOfficeType).SingleOrDefault(filter);
+            }
+        }
+    }
+}
