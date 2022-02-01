e87dc98f57f302ea33466bb1c692f2382edccef6
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-02-01 11:23:57 +0300

    ProcessType added to the WebApi Layer

---
+++ WebAPI/Controllers/ProcessTypesController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessTypesController : ControllerBase
+    {
+        private IProcessTypeService _processTypeService;
+
+        public ProcessTypesController(IProcessTypeService processTypeService)
+        {
+            _processTypeService = processTypeService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _processTypeService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _processTypeService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("ChangeActivity")]
+        public IActionResult ChangeActivity(int id)
+        {
+            var result = _processTypeService.ChangeActivity(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
