9ed7ffeade902222114e12f5089dc3aabe35f547
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-02-01 11:56:30 +0300

    TaskType added to the Business layer

---
+++ Business/Abstract/ITaskTypeService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ITaskTypeService
+    {
+        IDataResult<List<TaskType>> GetAll();
+        IDataResult<TaskType> GetById(int id);
+        IDataResult<List<TaskType>> GetAllByLicenceId(int licenceId);
+        IResult Add(TaskType taskType);
+        IResult Update(TaskType taskType);
+        IResult Delete(int id);
+        IResult ChangeActivity(int id);
+
+    }
+}
---
+++ Business/Concrete/TaskTypeManager.cs
@@ -0,0 +1,72 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class TaskTypeManager : ITaskTypeService
+    {
+        private ITaskTypeDal _taskTypeDal;
+
+        public TaskTypeManager(ITaskTypeDal taskTypeDal)
+        {
+            _taskTypeDal = taskTypeDal;
+        }
+
+        public IResult Add(TaskType taskType)
+        {
+            _taskTypeDal.Add(taskType);
+            return new SuccessResult(Messages.AddedSuccessfuly);
+        }
+
+        public IResult ChangeActivity(int id)
+        {
+            var taskType = _taskTypeDal.Get(tp => tp.TaskTypeId == id);
+            if (taskType == null)
+                return new ErrorResult(Messages.TheItemDoesNotExists);
+            taskType.IsActive = !taskType.IsActive;
+            var result = Update(taskType);
+            if (!result.Success)
+                return result;
+            return new SuccessResult(Messages.ActivityChangedSuccessfuly);
+        }
+
+        public IResult Delete(int id)
+        {
+            var taskType = _taskTypeDal.Get(tp => tp.TaskTypeId == id);
+            if (taskType == null)
+                return new ErrorResult(Messages.TheItemDoesNotExists);
+            _taskTypeDal.Delete(taskType);
+            return new SuccessResult(Messages.DeletedSuccessfuly);
+        }
+
+        public IDataResult<List<TaskType>> GetAll()
+        {
+            return new SuccessDataResult<List<TaskType>>(_taskTypeDal.GetAll(), Messages.GetAllSuccessfuly);
+        }
+
+        public IDataResult<List<TaskType>> GetAllByLicenceId(int licenceId)
+        {
+            return new SuccessDataResult<List<TaskType>>(_taskTypeDal.GetAll(tp => tp.LicenceId == licenceId), Messages.GetAllByLicenceIdSuccessfuly);
+        }
+
+        public IDataResult<TaskType> GetById(int id)
+        {
+            var taskType = _taskTypeDal.Get(tp => tp.TaskTypeId == id);
+            if (taskType == null)
+                return new ErrorDataResult<TaskType>(Messages.TheItemDoesNotExists);
+            return new SuccessDataResult<TaskType>(taskType, Messages.GetByIdSuccessfuly);
+        }
+
+        public IResult Update(TaskType taskType)
+        {
+            _taskTypeDal.Update(taskType);
+            return new SuccessResult(Messages.UpdatedSuccessfuly);
+        }
+    }
+}
