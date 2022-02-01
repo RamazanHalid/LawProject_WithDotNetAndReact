cba6a0c0039320082137255f1ad6bd2c3471e221
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-02-01 11:23:40 +0300

    ProcessType added to the Business Layer

---
+++ Business/Abstract/IProcessTypeService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProcessTypeService
+    {
+        IDataResult<List<ProcessType>> GetAll();
+        IDataResult<ProcessType> GetById(int id);
+        IResult Add(ProcessType processType);
+        IResult Update(ProcessType processType);
+        IResult Delete(int id);
+        IResult ChangeActivity(int id);
+    }
+}
---
+++ Business/Concrete/ProcessTypeManager.cs
@@ -0,0 +1,59 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProcessTypeManager : IProcessTypeService
+    {
+        private IProcessTypeDal _processTypeDal;
+
+        public ProcessTypeManager(IProcessTypeDal processTypeDal)
+        {
+            _processTypeDal = processTypeDal;
+        }
+
+        public IResult Add(ProcessType processType)
+        {
+            _processTypeDal.Add(processType);
+            return new SuccessResult(Messages.AddedSuccessfuly);
+        }
+
+        public IResult ChangeActivity(int id)
+        {
+            var processType = GetById(id);
+            if (!processType.Success)
+                return new ErrorResult(processType.Message);
+            processType.Data.IsActive = !processType.Data.IsActive;
+            var result = Update(processType.Data);
+            if (!result.Success)
+                return result;
+            return new SuccessResult(Messages.ActivityChangedSuccessfuly);
+        }
+
+        public IResult Delete(int id)
+        {
+            var processType = _processTypeDal.Get(pt => pt.ProcessTypeId == id);
+            _processTypeDal.Delete(processType);
+            return new SuccessResult(Messages.DeletedSuccessfuly);
+        }
+        public IDataResult<List<ProcessType>> GetAll()
+        {
+            return new SuccessDataResult<List<ProcessType>>(_processTypeDal.GetAll(), Messages.GetAllSuccessfuly);
+        }
+        public IDataResult<ProcessType> GetById(int id)
+        {
+            return new SuccessDataResult<ProcessType>(_processTypeDal.Get(pt => pt.ProcessTypeId == id), Messages.GetByIdSuccessfuly);
+        }
+        public IResult Update(ProcessType processType)
+        {
+            _processTypeDal.Update(processType);
+            return new SuccessResult(Messages.UpdatedSuccessfuly);
+        }
+    }
+}
