8389ed001ccf46b74e134eeb24f658beff151080
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-02-02 18:24:44 +0300

    TransactionActivityType added to the Business Layer

---
+++ Business/Abstract/ITransactionActivityTypeService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ITransactionActivityTypeService
+    {
+        IDataResult<List<TransactionActivityType>> GetAll();
+        IDataResult<TransactionActivityType> GetById(int id);
+        IResult Add(TransactionActivityType transactionActivityType);
+        IResult Update(TransactionActivityType transactionActivityType);
+        IResult Delete(int id);
+    }
+}
---
+++ Business/Concrete/TransactionActivityTypeManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class TransactionActivityTypeManager : ITransactionActivityTypeService
+    {
+        private ITransactionActivityTypeDal _transactionActivityTypeDal;
+
+        public TransactionActivityTypeManager(ITransactionActivityTypeDal transactionActivityTypeDal)
+        {
+            _transactionActivityTypeDal = transactionActivityTypeDal;
+        }
+
+        public IResult Add(TransactionActivityType transactionActivityType)
+        {
+            _transactionActivityTypeDal.Add(transactionActivityType);
+            return new SuccessResult(Messages.AddedSuccessfuly);
+        }
+
+        public IResult Delete(int id)
+        {
+            var getById = GetById(id);
+            if (!getById.Success)
+                return new ErrorResult(getById.Message);
+            var transactionActivityType = getById.Data;
+            _transactionActivityTypeDal.Delete(transactionActivityType);
+            return new SuccessResult(Messages.DeletedSuccessfuly);
+        }
+
+
+        public IDataResult<List<TransactionActivityType>> GetAll()
+        {
+            return new SuccessDataResult<List<TransactionActivityType>>(_transactionActivityTypeDal.GetAll(), Messages.GetAllSuccessfuly);
+        }
+
+        public IDataResult<TransactionActivityType> GetById(int id)
+        {
+            var transactionActivityType = _transactionActivityTypeDal.Get(t => t.TransactionActivityTypeId == id);
+            if (transactionActivityType == null)
+                return new ErrorDataResult<TransactionActivityType>(Messages.TheItemDoesNotExists);
+            return new SuccessDataResult<TransactionActivityType>(transactionActivityType, Messages.GetByIdSuccessfuly);
+        }
+
+        public IResult Update(TransactionActivityType transactionActivityType)
+        {
+            _transactionActivityTypeDal.Update(transactionActivityType);
+            return new SuccessResult(Messages.UpdatedSuccessfuly);
+        }
+    }
+}
