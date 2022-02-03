908fb7f130da72266e5fbf3cbb8f835922121cdd
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-02-03 20:38:40 +0300

    UnApprovedAccountException added to the ExceptionMiddleware

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Utilities.CustomExceptions;
 using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
@@ -46,13 +47,26 @@ namespace Core.Extensions
                 return httpContext.Response.WriteAsync(new ValidationErrorDetails
                 {
                     StatusCode = 400,
+                    SituationCode = 2,
                     Message = message,
                     Errors = errors
+                }.ToString());
+            }
+            if (e.GetType() == typeof(UnApprovedAccountException))
+            {
+                return httpContext.Response.WriteAsync(new UnApprovedAccountErrorDetails
+                {
+                    UserId = ((UnApprovedAccountException)e).UserId,
+                    Success = false,
+                    SituationCode = 3,
+                    StatusCode = httpContext.Response.StatusCode,
+                    Message = "UnAprovement account, please approve your account with Sms code!"
                 }.ToString());
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
             {
+                SituationCode = 1,
                 Success = false,
                 StatusCode = httpContext.Response.StatusCode,
                 Message = e.Message
