b01c5e79d4fda67c31b4bcff439b8615189ef890
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-13 17:19:19 +0300

    Account activity Type Entity added

---
+++ Entities/DTOs/AccountActivityType/AccountActivityTypeAddDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs.AccountActivityType
+{
+    public class AccountActivityTypeAddDto : IDto
+    {
+        public string Name { get; set; }
+    }
+}
