3df5735245f249930b18d6ec8ef96e71cb38b620
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-04-19 16:24:56 +0300

    NotificationController added

---
+++ WebAPI/Controllers/NotificationsController.cs
@@ -0,0 +1,93 @@
+﻿using Business.Abstract;
+using Entities;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+
+        [HttpGet("MakeItRead")]
+        public IActionResult MakeItRead(int id)
+        {
+            var result = _notificationService.MakeItRead(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _notificationService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _notificationService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("MakeAllItRead")]
+        public IActionResult MakeAllItRead()
+        {
+            var result = _notificationService.MakeItReadAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _notificationService.Delete(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("DeleteAll")]
+        public IActionResult DeleteAll()
+        {
+            var result = _notificationService.DeleteAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetCount")]
+        public IActionResult GetCount()
+        {
+            var result = _notificationService.GetCount();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
